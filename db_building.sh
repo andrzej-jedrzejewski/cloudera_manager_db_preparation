8044d104012ca621075e44c48c44c2892f841bab
Andrzej <a.jedrzejewski@kainos.com>
2016-10-26 15:18:11 +0100

    add mysql secure installation proccess

--- db_building.sh
+++ db_building.sh
@@ -10,7 +10,24 @@ mkdir -p /root/mysqlbk/
 mv /var/lib/mysql/ib_logfile0 /root/mysqlbk/
 mv /var/lib/mysql/ib_logfile1 /root/mysqlbk/
 cp my.cnf /etc/my.cnf
+systemctl stop start
 sudo /sbin/chkconfig mysqld on 
+
+
+echo "Start mysql secure installation proccess"
+mysql_secure_installation <<EOF
+
+y
+secret
+secret
+y
+y
+y
+y
+EOF
+
+echo "Secure installation proccess has been done"
+
 wget http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.40.tar.gz
 tar zxvf mysql-connector-java-5.1.40.tar.gz
 sudo mkdir -p /usr/share/java
