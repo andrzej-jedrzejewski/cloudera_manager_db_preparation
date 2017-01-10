1130daf8f2c2c49f215cc7be8021227734219359
Andrzej <a.jedrzejewski@kainos.com>
2017-01-10 10:57:49 +0000

    change name of nav db

--- db_building.sh
+++ db_building.sh
@@ -49,8 +49,8 @@ mysql --user=root --password=${db_root_password}<<_EOF_
   grant all on hivemetastore.* TO 'hive'@'%' IDENTIFIED BY '${hive_password}';
   create database sentry DEFAULT CHARACTER SET utf8;
   grant all on sentry.* TO 'sentry'@'%' IDENTIFIED BY '${sentry_password}';
-  create database nav DEFAULT CHARACTER SET utf8;
-  grant all on nav.* TO 'nav'@'%' IDENTIFIED BY '${nav_password}';
+  create database navaudit DEFAULT CHARACTER SET utf8;
+  grant all on navaudit.* TO 'navaudit'@'%' IDENTIFIED BY '${nav_password}';
   create database navms DEFAULT CHARACTER SET utf8;
   grant all on navms.* TO 'navms'@'%' IDENTIFIED BY '${navms_password}';
   create database oozie DEFAULT CHARACTER SET utf8;
